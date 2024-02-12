83bb8b189c4b1f77293a640187876fa92188d146
Chirag Bajaj <chirag.bajaj@hcode.tech>
2024-02-12 17:47:34 +0530

    The form validation is completed to refer documentation

--- reviews/views.py
+++ reviews/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
-
+from . import forms
 # Create your views here.
 def index(request):
-    if request.method=='POST':
-        enteredusername=request.POST['username']
-        print(enteredusername)
-        return HttpResponseRedirect('/ThankYou')
-    return render(request,"reviews/index.html")
+    if request.method == 'POST':
+        form = forms.ReviewForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("/ThankYou")
+    else:   
+        form = forms.ReviewForm()
+    return render(request,"reviews/index.html",{
+        'form':form
+    })
 def Thankspage(request):
-    return render(request,"reviews/Thankyou.html")
+    return render(request,"reviews/Thankyou.html") 
