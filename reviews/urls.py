ed2c629c446be37106d0c1d414befa05db64f33f
Chirag Bajaj <chirag.bajaj@hcode.tech>
2024-02-13 15:59:06 +0530

    created a new template view

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.reviewView.as_view()),
-    path('ThankYou',views.Thankspage)
+    path('ThankYou',views.ThankYouView.as_view()),
+    path('All',views.ReviewList.as_view())
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,10 @@
+from typing import Any
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse,HttpResponseRedirect
 from . import forms
 from .models import Review
 from django.views import View
+from django.views.generic.base import TemplateView
 # Create your views here.
 class reviewView(View):
     def get(slef,request):
@@ -20,18 +22,19 @@ class reviewView(View):
         return render(request,"reviews/index.html",{
         'form':form
     })
+class ThankYouView(TemplateView):
+    template_name='reviews/Thankyou.html'
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['message']='This Works'
+        return context
+    
+class ReviewList(TemplateView):
+    template_name='reviews/reviewslist.html'
 
-
-def index(request):
-    if request.method == 'POST':
-        form = forms.Reviewdb(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/ThankYou")
-    else:   
-        form = forms.Reviewdb()
-    return render(request,"reviews/index.html",{
-        'form':form
-    })
-def Thankspage(request):
-    return render(request,"reviews/Thankyou.html")
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        reviews=Review.objects.all()
+        context["reviews"]=reviews
+        return context
+    
