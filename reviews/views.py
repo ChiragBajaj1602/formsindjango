7480d8d62f5333258ed10ad9f75c24df930ebb86
Chirag Bajaj <chirag.bajaj@hcode.tech>
2024-02-13 13:14:09 +0530

    added the class based views for the forms

--- reviews/views.py
+++ reviews/views.py
@@ -2,13 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from . import forms
 from .models import Review
+from django.views import View
 # Create your views here.
+class reviewView(View):
+    def get(slef,request):
+        form = forms.Reviewdb()
+        return render(request,"reviews/index.html",{
+            "form":form
+        })
+    def post(self,request):
+        form = forms.Reviewdb(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/ThankYou")
+        else:   
+            form = forms.Reviewdb()
+        return render(request,"reviews/index.html",{
+        'form':form
+    })
+
+
 def index(request):
     if request.method == 'POST':
         form = forms.Reviewdb(request.POST)
         if form.is_valid():
-            review=Review(user_name=form.cleaned_data['user_name'],review_text=form.cleaned_data['review_text'],rating=form.cleaned_data['rating'])
-            review.save()
+            form.save()
             return HttpResponseRedirect("/ThankYou")
     else:   
         form = forms.Reviewdb()
@@ -16,4 +34,4 @@ def index(request):
         'form':form
     })
 def Thankspage(request):
-    return render(request,"reviews/Thankyou.html") 
+    return render(request,"reviews/Thankyou.html")
